1e29babae7feae428374247c98b20d20b80b84d9
Muhammad Zain <zmunawar872@gmail.com>
2026-07-30 15:33:14 +0500

    fix: update CORS origin to allow multiple local development URLs

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-    origin: "http://localhost:5174",
+    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
     credentials: true
 }));
 app.use(express.static('public'));
